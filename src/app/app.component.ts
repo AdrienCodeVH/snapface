367dbd9742b4948496110e7e2bfafdd0b74c8444
VOM HOEVEL <adriencodeprog@gmail.com>
2025-03-19 23:00:16 +0100

    Feature/Partie2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
+import { FaceSnap } from './models/face-snap';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,23 @@ import { FaceSnapComponent } from './face-snap/face-snap.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  mySnap!: FaceSnap;
+  myOtherSnap!: FaceSnap;
+
+  ngOnInit(): void {
+    this.mySnap = new FaceSnap(
+      'Archibald', 
+      'Mon meilleur ami depuis tout petit !',
+      new Date(),
+      0, 
+      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
+      );
+      this.myOtherSnap = new FaceSnap(
+        'Tree Rock Moutain', 
+        'Un beau paysage',
+        new Date(),
+        0, 
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Mount_Everest_from_Druk_Air_Plane_%2824604783380%29.jpg/1280px-Mount_Everest_from_Druk_Air_Plane_%2824604783380%29.jpg');
+  }
 }
---
+++ src/app/models/face-snap.ts
@@ -0,0 +1,17 @@
+export class FaceSnap {
+    constructor(
+        public title: string,
+        public description: string,
+        public createdAt: Date,
+        public snaps: number,
+        public imageUrl: string
+    ) {}
+
+    addSnap(): void {
+        this.snaps++;
+    }
+
+    removeSnap(): void {
+        this.snaps--;
+    }
+}
